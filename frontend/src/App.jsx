fa161aefd461c8ee8ef7c7d21b58228af2ded594
Sandesh-Huli <sandeshhuli25@gmail.com>
2025-09-21 12:48:26 +0530

    App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState, useContext } from 'react'
-import Navbar from './components/Navbar.jsx'
+import { useContext, useState } from 'react'
 import AppContextProvider from './context/AppContext.jsx'
 import { AppContext } from './context/AppContext.jsx';
+import Navbar from './components/Navbar.jsx'
+import Sidebar from './components/Sidebar.jsx'
+import Body from './components/Body.jsx';
 
-function App() {
-  const { showLogin, user, setUser, showProfile } = useContext(AppContext);
-  return (
+export default function App() {
+  const {showSidebar} = useContext(AppContext);
+  return (  
     <>
       <Navbar />
+      <div className="flex transition-all duration-300">
+        {showSidebar && <Sidebar />}
+        <div className={showSidebar===true ? "flex-1 ml-0 md:ml-0 transition-all duration-300" : "flex-1 transition-all duration-300"}>
+          <Body />
+        </div>
+      </div>
     </>
   )
 }
-
-export default App
