717809869b8b9a90546c648599219bdb3adfc106
Sandesh-Huli <sandeshhuli25@gmail.com>
2025-09-22 20:17:32 +0530

    Added auth icons

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,14 @@
+import close from './close.png'
+import email from './email.png'
+import hide from './hide.png'
+import logo from './logo.png'
+import padlock from './padlock.png'
+import plus from './plus.png'
+import send from './send.png'
+import show from './show.png'
+import sidebar from './sidebar.png'
+import user from './user.png'
+
+export const assets = {
+    close,email,hide,logo,padlock,plus,send,show,sidebar,user
+};
